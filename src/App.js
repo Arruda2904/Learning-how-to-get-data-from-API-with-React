bf301c283e27d86eb98b2bf8368f921d03ecb4e7
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-03-16 20:38:13 -0300

    Video 5 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React/*,{Component}*/ from 'react'
+import api from './api'
 
 function App() {
+  let [responseData, setResponseData] = React.useState([]);
+
+  async function pegaDado(){
+    const response = await api.get('star wars')
+    setResponseData(response.data)
+  }
+
+  /*React.useEffect( () => {
+    console.log(responseData)
+  },[responseData, setResponseData]);*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Listar os filmes</h1>
+      <button name="botao" type='button' onClick={pegaDado}>Botao</button>
+      {responseData.map(data => (
+        <li key={data.show.id}>
+          <h2>Titulo do filme: {data.show.name}</h2>
+          <p>{data.show.summary} </p>
+        </li>
+      ))}
     </div>
   );
 }
+
+/*class App extends Component {
+  state={
+    filmes: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('')
+    this.setState({ filmes: response.data})
+    console.log(response.data)
+  }
+  render() {
+    return(
+      <h1>Lista de filmes</h1>
+    )
+  }
+}*/
 
 export default App;
